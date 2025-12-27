3bf18386247ae708d885d841aeee3b3ad25c1727
Emre Yılmaz <yilmazemrecu@gmail.com>
2025-12-27 22:37:03 +0300

    project added from start.spring.io

---
+++ pageable/src/test/java/com/emrecanyilmaz/pageable/PageableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emrecanyilmaz.pageable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
